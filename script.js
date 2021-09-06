2ed8f9a0878cc1f2f22019851c91a175020cd4b3
AndyR <50073313+Andy-Robertson@users.noreply.github.com>
2021-09-06 11:18:36 +0100

    Level 100 Complete
    All level 100 requirements met.
    Completed a very basic style pass, prioritised functionality at this point.
    Additionally added a normalise script to ensure consistent CSS formatting going forward.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -6,7 +5,56 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  rootElem.className = "root";
+
+  let header = document.createElement("header");
+  rootElem.appendChild(header);
+  header.className = "header";
+  header.innerText = "HEADER";
+
+  let episodeListContainer = document.createElement("div");
+  rootElem.appendChild(episodeListContainer);
+  episodeListContainer.className = "episodeListContainer";
+
+  // function to iterate through `episodeList` and render episode cards
+  episodeList.forEach((episode) => {
+    let card = document.createElement("article");
+    episodeListContainer.appendChild(card);
+    card.className = "episodeCard";
+
+    let cardTitle = document.createElement("h2");
+    card.appendChild(cardTitle);
+    cardTitle.innerText = episode.name;
+
+    let episodeCode = "";
+
+    if (episode.season < 10) {
+      episodeCode += `S${episode.season.toString().padStart(2, "0")}`;
+    }
+
+    if (episode.number < 10) {
+      episodeCode += `E${episode.number.toString().padStart(2, "0")}`;
+    }
+
+    let combinedEpisodeSeason = document.createElement("div");
+    card.appendChild(combinedEpisodeSeason);
+    combinedEpisodeSeason.innerText = episodeCode;
+
+    let img = document.createElement("img");
+    card.appendChild(img);
+    img.src = episode.image.medium;
+    img.alt = `${episode.name} front cover`;
+
+    let summary = document.createElement("div");
+    card.appendChild(summary);
+    summary.innerHTML = episode.summary;
+  });
+
+  let footer = document.createElement("footer");
+  rootElem.appendChild(footer);
+  footer.className = "footer";
+  footer.innerHTML =
+    "<ul><li>All episode data from: <a href='https://www.tvmaze.com/api#licensing'>TVMaze.com</a></li></ul>";
 }
 
 window.onload = setup;
